32995f3a2da3c017a5f3151d131c15a940ec8164
Vinayak <vinugh@yahoo.in>
2019-12-29 02:03:21 +0530

    Commit

--- src/com/Test/NewTest.java
+++ src/com/Test/NewTest.java
@@ -1,9 +1,41 @@
 package com.Test;
 
-import org.testng.annotations.Test;
+//Java program to implement the above approach 
+import java.util.*; 
 
-public class NewTest {
-  @Test
-  public void f() {
-  }
-}
+
+class NewTest { 
+
+//Static function declared to find 
+//the encrypted string 
+	public static void findWord(String c, int n) { 
+		int co = 0, i; 
+
+		// Character array to store 
+		//the encrypted string 
+		char s[] = new char[n]; 
+   
+		for (i = 0; i < n ; i++) { 
+			if (i < n / 2) 
+				co++; 
+			else
+				co = n - i; 
+
+			// after 'z', it should go to a. 
+			if ((c.charAt(i) + co) <= 122) 
+				s[i] = (char)((int)c.charAt(i) + co); 
+			else
+				s[i] = (char)((int)c.charAt(i) + co - 26); 
+		} 
+   
+ // storing the character array in the string. 
+		String str = Arrays.toString(s); 
+		System.out.println(str); 
+	} 
+
+	//Driver code 
+//	public static void main(String args[]) { 
+//		String s = "abcd"; 
+//		findWord(s, s.length()); 
+//	} 
+} 
